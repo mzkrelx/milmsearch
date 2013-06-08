381c24859ac6e020b1cc4dfe8d15e413e485e781
Mizuki Yamanaka <charles.vineyard@gmail.com>
2013-06-09 03:00:22 +0900

    fix appName and appVersion

--- project/Build.scala
+++ project/Build.scala
@@ -4,8 +4,8 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "milm-search-ui-scala"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "milmsearch"
+  val appVersion      = "0.2-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
