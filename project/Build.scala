fcb460d9271b1473fb37ed0570571d09b3f23af7
Yamanaka Yuu <yuu@relx.jp>
2013-06-10 23:57:55 +0900

    Add PostgreSQL JDBC library dependency.
    * version -> 9.1-901.jdbc4

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,8 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "postgresql" % "postgresql" % "9.1-901.jdbc4"
   )
 
 
