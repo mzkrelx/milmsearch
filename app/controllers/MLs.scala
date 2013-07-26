a46d906c4fb8191eeecbef094d41b3832758ffe5
Mizuki Yamanaka <charles.vineyard@gmail.com>
2013-07-26 17:53:45 +0900

    Develop ml list.

--- app/controllers/MLs.scala
+++ app/controllers/MLs.scala
@@ -2,12 +2,28 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import controllers.Defaults._
 import models.MLProposal
+import models.{MLProposalStatus => MLPStatus}
+import utils.BadRequestException
+import models.ML
 
 object MLs extends Controller {
 
-  def list = Action {
-    Ok(views.html.mls.list())
+  def list(startIndex: Long, count: Int) = TryCatch4xx {
+    Action { implicit request =>
+
+      if (startIndex < 0 || count < 0) {
+        throw BadRequestException("`startIndex` and `count` must be positive number.")
+      }
+
+      val itemCountPerPage = count min MaxItemsPerPage
+      val mls = ML.list(startIndex, itemCountPerPage)
+
+      Ok(views.html.mls.list(
+        Page(mls, ML.totalResultCount, startIndex, itemCountPerPage)
+      ))
+    }
   }
 
 }
