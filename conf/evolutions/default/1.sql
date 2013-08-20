59ab29f331ed1eaa0e7bf8bf567d901d311b9562
Mizuki Yamanaka <charles.vineyard@gmail.com>
2013-08-20 16:57:10 +0900

    Fix DDL, add archive_type.
    Fix CONSTRAINT etc.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,19 +4,23 @@
 
 CREATE SEQUENCE ml_proposal_id_seq;
 CREATE TABLE ml_proposal (
-    id             INTEGER      DEFAULT nextval('ml_proposal_id_seq') PRIMARY KEY,
+    id             BIGINT       DEFAULT nextval('ml_proposal_id_seq'),
     proposer_name  VARCHAR(100) NOT NULL,
     proposer_email VARCHAR(100) NOT NULL,
     ml_title       VARCHAR(100) NOT NULL,
-    status         VARCHAR(20)  NOT NULL CHECK (status in ('new', 'accepted', 'rejected')),
-    archive_type   VARCHAR(20)  NOT NULL CHECK (archive_type in ('mailman', 'other')),
+    status         VARCHAR(20)  NOT NULL,
+    archive_type   VARCHAR(20)  NOT NULL,
     archive_url    VARCHAR(200) NOT NULL,
     message        TEXT         NOT NULL,
     judged_at      TIMESTAMP,
     created_at     TIMESTAMP    NOT NULL,
-    updated_at     TIMESTAMP    NOT NULL
+    updated_at     TIMESTAMP    NOT NULL,
+    CONSTRAINT ml_proposal_pkey PRIMARY KEY (id),
+    CONSTRAINT ml_proposal_status_check CHECK (status in ('new', 'accepted', 'rejected')),
+    CONSTRAINT ml_proposal_archive_type_check CHECK (archive_type in ('mailman', 'other'))
 );
 
 # --- !Downs
 
 DROP TABLE ml_proposal;
+DROP SEQUENCE ml_proposal_id_seq;
